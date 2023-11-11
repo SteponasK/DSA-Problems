dbc728942842b4126caf447d9123613baecd8813
SteponasK <132608893+SteponasK@users.noreply.github.com>
2023-11-11 21:47:02 +0200

    Fixed a bug (multiplications instead of addition/ edge case added

--- Maximum-Product/O(n^2)/main.cpp
+++ Maximum-Product/O(n^2)/main.cpp
@@ -8,12 +8,14 @@ int main() {
 	return 0;
 }
 void printMaxProduct(const std::vector<int>& nums) {
+	if (nums.size() < 2)
+		return;
 	int max{ nums[0] }; // default max value
 	int maxFirst{ 0 };
 	int maxSecond{ 0 };
 	for (int i = 0; i < nums.size(); ++i) {
 		for (int j = i+1; j < nums.size(); ++j) {
-			if (nums[i] + nums[j] > max) { 
+			if (nums[i] * nums[j] > max) { 
 				max = nums[i] + nums[j];
 				maxFirst = nums[i];
 				maxSecond = nums[j];
